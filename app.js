0cbdfe4583e3f523d97bffb5b3f6cf8961223111
Dileep9098 <dileeprathore9098@gmail.com>
2026-04-04 16:18:54 +0530

    09.01.02

--- app.js
+++ app.js
@@ -223,13 +223,13 @@ app.use(cors({
   }
 }));
 
-
-// app.use(express.static(path.join(__dirname, "/bulk-message/dist")));
-// app.use('/upload', express.static(path.join(__dirname, 'upload')));
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));  
-
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Serve static files with proper CORS headers
+app.use('/uploads', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}, express.static(path.join(__dirname, 'uploads')));
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
